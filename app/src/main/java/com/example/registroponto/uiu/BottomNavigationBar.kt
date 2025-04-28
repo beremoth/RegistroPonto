6c3d58909ad24fc01f910956affc877e714ad77f
beremoth <luizeduardotheodoro3@gmail.com>
2025-04-28 14:00:54 -0300

    implementação no bottomnavigation, ainda sem sucesso

--- app/src/main/java/com/example/registroponto/uiu/BottomNavigationBar.kt
+++ app/src/main/java/com/example/registroponto/uiu/BottomNavigationBar.kt
@@ -1,9 +1,6 @@
 package com.example.registroponto.uiu
 
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
